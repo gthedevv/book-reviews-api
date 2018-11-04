6e6a3bcf7ab74ef25c58dbbb264d488ff289d13a
Gerard Fajardo <gedfaj@gmail.com>
2018-11-04 10:40:13 -0800

    Rename config.js to index.js

---
+++ config/index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const config = {
+  production: {
+    DATABASE: 'mongodb://gerard:gfajardo0@ds151863.mlab.com:51863/my-book-reviews',
+    PORT: process.env.PORT || 5000,
+    SECRET: process.env.JWT_SECRET,
+    EXPIRY: process.env.JWT_EXPIRY || '1h'
+  },
+  default:{
+    DATABASE: 'mongodb://localhost:27017/myBookReviews',
+    PORT: process.env.PORT || 5000,
+    SECRET: process.env.JWT_SECRET,
+    EXPIRY: process.env.JWT_EXPIRY || '1h'
+  }
+}
+
+exports.get = function get(env){
+  return config[env] || config.default
+}
