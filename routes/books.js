47e30447e101a1ac6ce2456707c9bf63ca832196
Gerard Fajardo <gedfaj@gmail.com>
2018-11-04 23:25:24 -0800

    Add put and delete routes for books api.

--- routes/books.js
+++ routes/books.js
@@ -35,32 +35,67 @@ router.get('/books', (req, res) => {
 router.post('/book', (req, res) => {
   const { name, author, review, pages, rating, price, reviewerId } = req.body
 
-    const requiredFields = ['name', 'author', 'review', 'pages', 'rating', 'price', 'reviewerId']
-    const missingField = requiredFields.find(field => !(field in req.body))
+  const requiredFields = ['name', 'author', 'review', 'pages', 'rating', 'price', 'reviewerId']
+  const missingField = requiredFields.find(field => !(field in req.body))
 
-    if(missingField) {
-      return res.status(422).send({
-        code: 422,
-        reason: 'ValidationError',
-        message: 'Missing field',
-        location: missingField
-      });
+  if(missingField) {
+    return res.status(422).send({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Missing field',
+      location: missingField
+    });
+  }
+
+  Book.create({
+    name,
+    author,
+    review,
+    pages,
+    rating,
+    price,
+    reviewerId
+  })
+  .then(doc => res.status(201).send(doc.bookPosted()))
+  .catch(err => {
+    console.error(err);
+    res.status(500).send({message: 'Internal server error'})
+  });
+});
+
+  router.put('/book/:id', (req, res) => {
+    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+      res.status(400).send({
+        error: 'Request path id and request body id values must match'
+      })
     }
 
-    Book.create({
-      name,
-      author,
-      review,
-      pages,
-      rating,
-      price,
-      reviewerId
-    })
-    .then(doc => res.status(201).send(doc.bookPosted()))
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({message: 'Internal server error'})
+    const updated = {}
+
+    const updateableFields = ['name', 'author', 'review', 'pages', 'rating', 'price', 'reviewerId'];
+    updateableFields.forEach(field => {
+      if (field in req.body) {
+        updated[field] = req.body[field];
+      }
     });
+
+
+    Book
+    .findByIdAndUpdate(req.params.id, updated, { new: true })
+    .then(updatedBook => res.status(200).send({success:true, updatedBook}))
+    .catch(err => res.status(500).send({ message: 'Something went wrong' }));
+  });
+
+  router.delete('/book/:id', (req, res) => {
+    Book
+      .findByIdAndRemove(req.params.id)
+      .then(() => {
+        res.status(204).send({ message: 'success' });
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send({ error: 'something went terribly wrong' });
+      });
   });
 
 module.exports = router;
