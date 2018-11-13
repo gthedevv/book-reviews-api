12642f72232445ed0f82ad171f76b8c5e53d3f29
Gerard Fajardo <gedfaj@gmail.com>
2018-11-12 16:16:29 -0800

    Implement CORS on sever.

--- server.js
+++ server.js
@@ -23,6 +23,16 @@ app.use(bodyParser.json());
 // Logging
 app.use(morgan('common'));
 
+// CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
 // handle routes
 app.use('/users', users);
 app.use('/books', books);
