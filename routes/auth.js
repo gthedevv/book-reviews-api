16a71a9a14aa02c85625d3b2010e3e0f145a892e
Gerard Fajardo <gedfaj@gmail.com>
2018-11-12 17:16:26 -0800

    Add auth endpoint to routes.
    Auth endpoint will be used to authenticate users on login
    and refresh jwts.

---
+++ routes/auth.js
@@ -0,0 +1,33 @@
+'use strict';
+const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const { SECRET, EXPIRY } = require('../config').get(process.env.NODE_ENV);
+const router = express.Router();
+
+const createAuthToken = function(user) {
+
+  return jwt.sign({user}, SECRET, {
+    subject: user.username,
+    expiresIn: EXPIRY,
+    algorithm: 'HS256'
+  });
+};
+
+const localAuth = passport.authenticate('local', {session: false});
+// The user provides a username and password to login
+router.post('/login', localAuth, (req, res) => {
+  const authToken = createAuthToken(req.user);
+  res.send({authToken});
+});
+
+const jwtAuth = passport.authenticate('jwt', {session: false});
+
+// The user exchanges a valid JWT for a new one with a later expiration
+router.post('/refresh', jwtAuth, (req, res) => {
+  const authToken = createAuthToken(req.user);
+  res.send({authToken});
+});
+
+module.exports = router;
