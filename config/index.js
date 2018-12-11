ef015515e057ab396f720cb906d5e17754e85d33
Gerard Fajardo <gedfaj@gmail.com>
2018-12-10 22:14:47 -0800

    change JWT_EXPIRY to EXPIRY

--- config/index.js
+++ config/index.js
@@ -3,4 +3,4 @@ exports.DATABASE = process.env.DATABASE || 'mongodb://localhost:27017/myBookRevi
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/myBookReviews';
 exports.PORT = process.env.PORT || 5000;
 exports.SECRET = process.env.JWT_SECRET;
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+exports.EXPIRY = process.env.JWT_EXPIRY || '7d';
