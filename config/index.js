0e06caa12a9790f85913896353c5cc308ac01117
Gerard Fajardo <gedfaj@gmail.com>
2018-12-10 20:10:46 -0800

    update config chnage DATABASE_URL to DATABASE

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 'use strict';
-exports.DATABASE = process.env.DATABASE_URL || 'mongodb://localhost:27017/myBookReviews';
+exports.DATABASE = process.env.DATABASE || 'mongodb://localhost:27017/myBookReviews';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/myBookReviews';
 exports.PORT = process.env.PORT || 5000;
 exports.SECRET = process.env.JWT_SECRET;
